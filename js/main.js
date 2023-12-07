2a6c46454ce3f24ae05a9e77283c2216c018dfcc
ALPINISTLAR <niveam424@gmail.com>
2023-12-07 21:43:57 +0200

    add active style

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-document.addEventListener('keydown', function(event) {
-  handleAudio(event.keyCode || event.which);
-});
-
 document.querySelectorAll('.sound').forEach(function(element) {
+  // Click hodisasi
   element.addEventListener('click', function() {
       handleAudio(this.getAttribute('data-key'));
+      // Ustida hodisa amalga oshirilgan elementning stillarini o'zgartirish
+      this.classList.add('active');
+      setTimeout(function() {
+          element.classList.remove('active');
+      }, 200); // Misol uchun 200 millik vaqt
   });
+});
+
+// Keydown hodisasi
+document.addEventListener('keydown', function(event) {
+  var keyCode = event.keyCode || event.which;
+  var keyElement = document.querySelector('.sound[data-key="' + keyCode + '"]');
+
+  if (keyElement) {
+      handleAudio(keyCode);
+      // Ustida hodisa amalga oshirilgan elementning stillarini o'zgartirish
+      keyElement.classList.add('active');
+      setTimeout(function() {
+          keyElement.classList.remove('active');
+      }, 200); // Misol uchun 200 millik vaqt
+  }
 });
 
 function handleAudio(keyCode) {
