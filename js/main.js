c7491832a83082fc594bb05218936532892476a2
ALPINISTLAR <niveam424@gmail.com>
2023-12-07 22:06:47 +0200

    add title, info

--- js/main.js
+++ js/main.js
@@ -1,37 +1,35 @@
 document.querySelectorAll('.sound').forEach(function(element) {
-  // Click hodisasi
+
   element.addEventListener('click', function() {
-      handleAudio(this.getAttribute('data-key'));
-      // Ustida hodisa amalga oshirilgan elementning stillarini o'zgartirish
-      this.classList.add('active');
-      setTimeout(function() {
-          element.classList.remove('active');
-      }, 200); // Misol uchun 200 millik vaqt
+    handleAudio(this.getAttribute('data-key'));
+
+    this.classList.add('active');
+    setTimeout(function() {
+      element.classList.remove('active');
+    }, 200);
   });
 });
 
-// Keydown hodisasi
+
 document.addEventListener('keydown', function(event) {
   var keyCode = event.keyCode || event.which;
   var keyElement = document.querySelector('.sound[data-key="' + keyCode + '"]');
 
   if (keyElement) {
-      handleAudio(keyCode);
-      // Ustida hodisa amalga oshirilgan elementning stillarini o'zgartirish
-      keyElement.classList.add('active');
-      setTimeout(function() {
-          keyElement.classList.remove('active');
-      }, 200); // Misol uchun 200 millik vaqt
+    handleAudio(keyCode);
+    keyElement.classList.add('active');
+    setTimeout(function() {
+      keyElement.classList.remove('active');
+    }, 200);
   }
 });
 
 function handleAudio(keyCode) {
-  // Audio elementini tanlash
+
   var audioElement = document.querySelector('.audio audio[data-key="' + keyCode + '"]');
 
-  // Agar audio fayli topilsa, uni ijro etish
   if (audioElement) {
-      audioElement.currentTime = 0; // Qayta boshlash uchun
-      audioElement.play();
+    audioElement.currentTime = 0;
+    audioElement.play();
   }
 }
